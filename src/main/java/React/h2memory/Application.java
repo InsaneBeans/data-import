7341f2cfde59348212981c6c770fe4f303c9d585
huhong <huhong@bonc.com.cn>
2016-11-23 10:47:22 +0800

    缩减文件操作接口

---
+++ src/main/java/React/h2memory/Application.java
@@ -0,0 +1,16 @@
+package React.h2memory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *@author huh
+ *
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args){
+		SpringApplication.run(Application.class, args);
+	}
+}
--- src/test/java/React/h2memory/H2Test.java
+++ src/test/java/React/h2memory/H2Test.java
@@ -4,9 +4,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = App.class)
+@SpringApplicationConfiguration(classes = Application.class)
 public class H2Test {
 
 }
